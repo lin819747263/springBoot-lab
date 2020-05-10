7ea9abf3536f7a5161224d243f40059842cc6d2c
lin819747263 <819747263@qq.com>
2020-05-10 19:25:25 +0800

    补充

---
+++ springboot-mybatis/src/test/java/com/linmsen/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linmsen.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
