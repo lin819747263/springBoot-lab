7dee145a168c07d8941f0042130703d7a36860fa
lin819747263 <819747263@qq.com>
2020-05-23 15:14:56 +0800

    配置中心案例

---
+++ config-nacos/src/main/java/com/linmsen/confignacos/ConfigNacosApplication.java
@@ -0,0 +1,14 @@
+package com.linmsen.confignacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigNacosApplication.class, args);
+    }
+
+}
