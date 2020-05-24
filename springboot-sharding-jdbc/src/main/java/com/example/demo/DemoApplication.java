99bc46e1181c777f58f5e8105495b5b7a1933f26
lin819747263 <819747263@qq.com>
2020-05-24 10:26:25 +0800

    动态数据源案例增加

---
+++ springboot-sharding-jdbc/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.example.demo.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ springboot-sharding-jdbc/src/test/java/com/example/demo/mapper/OrderMapperTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.mapper;
+
+import com.example.demo.DemoApplication;
+import com.example.demo.dataobject.OrderDO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class OrderMapperTest {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Test
+    public void testSelectById() {
+        OrderDO order = orderMapper.selectById(1);
+        System.out.println(order.toString());
+    }
+
+}
